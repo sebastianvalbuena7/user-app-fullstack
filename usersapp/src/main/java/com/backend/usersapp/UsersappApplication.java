36184b8ab30bffb8fb7342ba6d64f0d8c4954932
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-05-10 13:10:55 -0500

    empezando el proyecto

---
+++ usersapp/src/main/java/com/backend/usersapp/UsersappApplication.java
@@ -0,0 +1,13 @@
+package com.backend.usersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersappApplication.class, args);
+	}
+
+}
